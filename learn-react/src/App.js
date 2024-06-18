492859bc1ac51c39ce082d17891ff922059cec1f
fabianflanhardt <fabian.flanhardt@pcg.io>
2024-06-18 20:49:42 +0000

    add sample data and accordion display

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,9 +1,33 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
-import { ChakraProvider, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { ChakraProvider, Menu, MenuButton, MenuList, MenuItem, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from "@chakra-ui/react";
+import { useState } from "react";
+
+const orders = [
+  {
+    orderNumber: 1,
+    itemName: "Item 1",
+    description: "Description for Item 1",
+    comment: "Comment for Item 1"
+  },
+  {
+    orderNumber: 2,
+    itemName: "Item 2",
+    description: "Description for Item 2",
+    comment: "Comment for Item 2"
+  },
+  {
+    orderNumber: 3,
+    itemName: "Item 3",
+    description: "Description for Item 3",
+    comment: "Comment for Item 3"
+  }
+];
+
 
 function HomePage() {
   return (
@@ -12,16 +36,73 @@ function HomePage() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Is it now?</p>
         <button>Click me!</button>
+
       </header>
     </div>
   );
 }
 
 function AnotherPage() {
+  const [editedOrders, setEditedOrders] = useState(orders);
+
+  const handleDescriptionChange = (index, event) => {
+    const updatedOrders = [...editedOrders];
+    updatedOrders[index].description = event.target.value;
+    setEditedOrders(updatedOrders);
+  };
+
+  const handleCommentChange = (index, event) => {
+    const updatedOrders = [...editedOrders];
+    updatedOrders[index].comment = event.target.value;
+    setEditedOrders(updatedOrders);
+  };
+
   return (
-
-    <PDFViewer src="./The Art and Science of Prompting.pdf" />
-
+    <div className="App-body">
+      <div style={{ width: '60%' }}>
+        <PDFViewer src="./The Art and Science of Prompting.pdf" />
+      </div>
+      <div style={{ width: '40%' }}>
+        <Accordion allowToggle>
+          {editedOrders.map((order, index) => (
+            <AccordionItem key={order.orderNumber}>
+              <h2>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">
+                    {order.itemName}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <table>
+                  <tr>
+                    <td>Description:</td>
+                    <td>
+                      <input
+                        type="text"
+                        value={order.description}
+                        onChange={(event) => handleDescriptionChange(index, event)}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Comment:</td>
+                    <td>
+                      <input
+                        type="text"
+                        value={order.comment}
+                        onChange={(event) => handleCommentChange(index, event)}
+                      />
+                    </td>
+                  </tr>
+                </table>
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div >
+    </div >
   );
 }
 
@@ -29,7 +110,7 @@ function AnotherPage() {
 
 function PDFViewer({ src }) {
   return (
-    <Box width="100%" height="600px">
+    <Box width="100%" height="90vh">
       <embed src={src} type="application/pdf" width="100%" height="100%" />
     </Box>
   );
